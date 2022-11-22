99e1faa3b876ddcc2e5e533f3c84cbc24f6b4df9
Vitor da Fonseca Rodrigues <vitorfon20@gmail.com>
2022-11-22 18:19:56 -0300

    correções 2

--- HeapSortMax.c
+++ HeapSortMax.c
@@ -4,7 +4,6 @@ typedef struct
 {
     int A[101];
     int n;
-    int tamanhoDoHeap;
 } HEAP;
 
 int pai(int i)
@@ -26,52 +25,52 @@ void refazHeapMaximo(HEAP *h, int i)
     int L = filho_Left(i);
     int R = filho_Right(i);
     int maior = 0;
-    if (L <= h->n && h->A[R] > h->A[i])
+    if (L <= h->n && h->A[L] > h->A[i])
         maior = L;
     else
         maior = i;
 
-    if (R <= h->tamanhoDoHeap && h->A[R] > h->A[maior])
+    if (R <= h->n && h->A[R] > h->A[maior])
         maior = R;
     if (maior != i)
     {
         int aux = h->A[i];
         h->A[i] = h->A[maior];
         h->A[maior] = aux;
-        refazHeapMaximo(&h, maior);
+        refazHeapMaximo(h, maior);
     }
 }
 void constroiHeapMaximo(HEAP *h)
 {
 
-    h->tamanhoDoHeap = h->n;
     for (int i = h->n / 2; i >= 1; i--)
     {
 
-        refazHeapMaximo(&h, i);
+        refazHeapMaximo(h, i);
     }
 }
 void HeapSort(HEAP *h)
 {
 
-    constroiHeapMaximo(&h);
+    constroiHeapMaximo(h);
     for (int i = (h->n); i >= 2; i--)
     {
 
         int aux = h->A[1];
         h->A[1] = h->A[i];
         h->A[i] = aux;
-        h->tamanhoDoHeap = (h->tamanhoDoHeap) - 1;
 
-        refazHeapMaximo(&h, 1);
+        refazHeapMaximo(h, 1);
     }
+}
+void exibir(HEAP h){
+
 }
 int main()
 {
     int vetor[5] = {5, 2, 3, 1, 4};
     HEAP h;
     h.n = 5;
-    h.tamanhoDoHeap = (h.n) / 2;
     for (int i = 1; i <= h.n; i++)
     {
         h.A[i] = vetor[i - 1];
@@ -83,4 +82,6 @@ int main()
     {
         printf("%d ", h.A[i]);
     }
+    //exibir(h);
+    
 }
